c0f253caf94c4f221ea0383162209776f299a0cc
hbach24 <hbach003@ucr.edu>
2021-11-11 15:27:52 -0800

    Add Select_And class and unit tests

--- select.hpp
+++ select.hpp
@@ -53,4 +53,28 @@ public:
 		return false;	
 	}
 };
+
+class Select_And : public Select 
+{
+private:
+	Select* s1;
+	Select* s2;
+
+public:
+	Select_And(Select* a, Select* b) {
+		s1 = a; s2 = b;
+	}
+	
+	virtual bool select(const Spreadsheet* sheet, int row) const {
+		if((s1->select(sheet, row)) && (s2->select(sheet, row))) {
+			return true;
+		}
+		return false;
+	}
+
+	 ~Select_And() {
+		delete s1;
+		delete s2;
+	}
+};
 #endif //__SELECT_HPP__
--- test.cpp
+++ test.cpp
@@ -63,11 +63,110 @@ TEST(Select_Contains, TwoLetters) {
 
 }
 
+TEST(Select_And, Contains) {
+	std::stringstream s;
 
+	Spreadsheet sheet;
 
+	sheet.set_column_names({"electronics"});
+	sheet.add_row({"laptop"});
+	sheet.add_row({"desktop"});
+	sheet.add_row({"mouse"});
 
+	sheet.set_selection(new Select_And(new Select_Contains(&sheet, "electronics", "mou"), new Select_Contains(&sheet, "electronics", "se")));
+ 	sheet.print_selection(s);
+	
+	EXPECT_EQ("mouse \n", s.str());
+} 
 
+TEST(Select_Contains, Basic) {
+	std::stringstream s;
+	std::string test;
 
+	Spreadsheet sheet;
+
+	sheet.set_column_names({"person"});
+	sheet.add_row({"Bob"});
+	sheet.add_row({"Mike"});
+
+	sheet.set_selection(new Select_Contains(&sheet, "person", "Mike"));
+	sheet.print_selection(s);
+	EXPECT_EQ("Mike \n", s.str());
+}
+
+TEST(ColumnTest, ColumnNotExists) {
+	Spreadsheet sheet;
+	bool valid = false;
+
+	sheet.set_column_names({"person"});
+	sheet.add_row({"Lee"});;
+	sheet.add_row({"Rob"});
+	sheet.add_row({"Sara"});
+
+	if(sheet.get_column_by_name("people") != -1) {
+		valid = true;
+	}
+	else {
+		valid = false;
+	}	
+
+	EXPECT_EQ(false, valid);	
+
+}
+
+TEST(ColumnTest, ColumnExists) {
+
+        Spreadsheet sheet;
+        bool valid = false;
+
+        sheet.set_column_names({"person"});
+        sheet.add_row({"Lee"});;
+        sheet.add_row({"Rob"});
+        sheet.add_row({"Sara"});
+
+        if(sheet.get_column_by_name("person") != -1) {
+                valid = true;
+        }
+        else {
+                valid = false;
+        }
+
+        EXPECT_EQ(true, valid);
+
+}
+
+TEST(Select_And, Empty) {
+	std::stringstream s;
+	
+	Spreadsheet sheet;
+
+	sheet.set_column_names({"fruit"});
+	sheet.add_row({"kiwi"});
+	sheet.add_row({"pear"});
+	sheet.add_row({"plum"});
+
+	sheet.set_selection(new Select_And(new Select_Contains(&sheet, "fruit", "pe"), new Select_Contains(&sheet, "fruit", "")));
+	sheet.print_selection(s);
+
+	EXPECT_EQ("pear \n", s.str());
+}
+
+TEST(Select_And, Case) {
+	std::stringstream s;
+
+	Spreadsheet sheet;
+
+	sheet.set_column_names({"fruit"});
+	sheet.add_row({"pear"});
+	sheet.add_row({"peach"});
+	sheet.add_row({"kiwi"});
+	sheet.add_row({"plum"});
+
+	sheet.set_selection(new Select_And(new Select_Contains(&sheet, "fruit", "KI"), new Select_Contains(&sheet, "fruit",  "wi")));
+	sheet.print_selection(s);
+
+	EXPECT_EQ("", s.str()); //expect an empty string since "KI" doesn't exist
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
