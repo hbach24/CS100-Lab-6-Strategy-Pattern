f26d6fe7ea74ded59eaa872df8dd945c3b62cdf8
hbach24 <hbach003@ucr.edu>
2021-11-11 19:40:27 -0800

    update unit tests

--- test.cpp
+++ test.cpp
@@ -3,66 +3,6 @@
 #include "gtest/gtest.h"
 #include "select.hpp"
 
-TEST(Select_Contains, ReturnTrue) {
-	Spreadsheet sheet;
-
-  	sheet.set_column_names({"fruit"});
-        sheet.add_row({"apple"});
-        sheet.add_row({"grapes"});
-        sheet.add_row({"peach"});
-        sheet.add_row({"orange"});
-
-        Select* test = new Select_Contains(&sheet , "fruit", "apple");
-
-        EXPECT_EQ(true, test->select(&sheet, 0));
-
-}
-
-TEST(Select_Contains, EmptyString) {
-        Spreadsheet sheet;
-
-        sheet.set_column_names({"fruit"});
-        sheet.add_row({"apple"});
-        sheet.add_row({"grapes"});
-        sheet.add_row({"peach"});
-        sheet.add_row({"orange"});
-
-        Select* test = new Select_Contains(&sheet , "fruit", "");
-
-        EXPECT_EQ(true, test->select(&sheet, 0));
-
-}
-
-TEST(Select_Contains, DifferentCase) {
-        Spreadsheet sheet;
-
-        sheet.set_column_names({"fruit"});
-        sheet.add_row({"apple"});
-        sheet.add_row({"grapes"});
-        sheet.add_row({"peach"});
-        sheet.add_row({"orange"});
-
-        Select* test = new Select_Contains(&sheet , "fruit", "Apple");
-
-        EXPECT_EQ(false, test->select(&sheet, 0));
-
-}
-
-TEST(Select_Contains, TwoLetters) {
-        Spreadsheet sheet;
-
-        sheet.set_column_names({"fruit"});
-        sheet.add_row({"apple"});
-        sheet.add_row({"grapes"});
-        sheet.add_row({"peach"});
-        sheet.add_row({"orange"});
-
-        Select* test = new Select_Contains(&sheet , "fruit", "pp");
-
-        EXPECT_EQ(true, test->select(&sheet, 0));
-
-}
-
 TEST(Select_And, Contains) {
 	std::stringstream s;
 
