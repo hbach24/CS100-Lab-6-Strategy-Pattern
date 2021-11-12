0d41dab6df47ba7a634990f6d68a0a51bfc3126d
JingwenHuang233 <jingwenhuang233@gmail.com>
2021-11-11 22:04:14 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,64 +4,85 @@
 #include "select.hpp"
 
 TEST(Select_Contains, ReturnTrue) {
+	std::stringstream s;
+	
 	Spreadsheet sheet;
 
   	sheet.set_column_names({"fruit"});
         sheet.add_row({"apple"});
         sheet.add_row({"grapes"});
         sheet.add_row({"peach"});
-        sheet.add_row({"orange"});
-
-        Select* test = new Select_Contains(&sheet , "fruit", "apple");
-
-        EXPECT_EQ(true, test->select(&sheet, 0));
+	
+	sheet.set_selection(new Select_Contains(&sheet, "fruit", "apple"));
+	sheet.print_selection(s);
+        EXPECT_EQ("apple \n", s.str());
 
 }
 
 TEST(Select_Contains, EmptyString) {
+        std::stringstream s;
+
         Spreadsheet sheet;
 
         sheet.set_column_names({"fruit"});
         sheet.add_row({"apple"});
         sheet.add_row({"grapes"});
         sheet.add_row({"peach"});
-        sheet.add_row({"orange"});
 
-        Select* test = new Select_Contains(&sheet , "fruit", "");
-
-        EXPECT_EQ(true, test->select(&sheet, 0));
+        sheet.set_selection(new Select_Contains(&sheet, "fruit", ""));
+        sheet.print_selection(s);
+        EXPECT_EQ("apple \ngrapes \npeach \n", s.str());
 
 }
 
-TEST(Select_Contains, DifferentCase) {
+TEST(Select_Contains, OneLetter) {
+        std::stringstream s;
+
+        Spreadsheet sheet;
+
+        sheet.set_column_names({"Name"});
+        sheet.add_row({"mike"});
+        sheet.add_row({"tony"});
+        sheet.add_row({"tom"});
+
+        sheet.set_selection(new Select_Contains(&sheet, "Name", "t"));
+        sheet.print_selection(s);
+        EXPECT_EQ("tony \ntom \n", s.str());
+
+}
+
+TEST(Select_Contains, NonExistingColumn) {
+        std::stringstream s;
+
         Spreadsheet sheet;
 
         sheet.set_column_names({"fruit"});
         sheet.add_row({"apple"});
         sheet.add_row({"grapes"});
         sheet.add_row({"peach"});
-        sheet.add_row({"orange"});
 
-        Select* test = new Select_Contains(&sheet , "fruit", "Apple");
-
-        EXPECT_EQ(false, test->select(&sheet, 0));
+        sheet.set_selection(new Select_Contains(&sheet, "food", "apple"));
+        sheet.print_selection(s);
+        EXPECT_EQ("\n", s.str());
 
 }
 
-TEST(Select_Contains, TwoLetters) {
+TEST(Select_Contains, TwoColumnWithSameName) {
+        std::stringstream s;
+
         Spreadsheet sheet;
 
-        sheet.set_column_names({"fruit"});
-        sheet.add_row({"apple"});
-        sheet.add_row({"grapes"});
-        sheet.add_row({"peach"});
-        sheet.add_row({"orange"});
+        sheet.set_column_names({"fruit", "fruit"});
+        sheet.add_row({"apple", "dragon fruit"});
+        sheet.add_row({"grapes", "orange"});
+        sheet.add_row({"peach", "avocado"});
 
-        Select* test = new Select_Contains(&sheet , "fruit", "pp");
-
-        EXPECT_EQ(true, test->select(&sheet, 0));
+        sheet.set_selection(new Select_Contains(&sheet, "fruit", "s"));
+        sheet.print_selection(s);
+        EXPECT_EQ("grapes orange \n", s.str());
 
 }
+
 
 
 
